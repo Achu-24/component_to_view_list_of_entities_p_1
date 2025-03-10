4ddb4955bc492d472e716e6ba952b26dbfe59ba6
Achu-24 <hamshaverthinik@gmail.com>
2025-03-10 10:18:17 +0530

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
-// App.jsx
-import React from "react";
-import BookCard from "./cart/BookCard";
+import { useState } from 'react';
 
-const books = [
-  { title: "1984", author: "George Orwell", genre: "Dystopian", year: 1949 },
-  { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Classic", year: 1960 },
-  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Classic", year: 1925 },
-];
+function App() {
+  const books = [
+    {
+      id: 1,
+      image:
+        'https://tse2.mm.bing.net/th?id=OIP.IIIPIPZYMBOJgxFfWGRtBAHaHa&rs=1&pid=ImgDetMain',
+      name: 'The Great Adventure',
+      genre: 'Fiction',
+      author: 'Jane Doe',
+    },
+    {
+      id: 2,
+      image: 'https://i.thenile.io/r1000/9781496687197.jpg?r=6085750d133cf',
+      name: 'Mysteries of the Universe',
+      genre: 'Science',
+      author: 'John Smith',
+    },
+    {
+      id: 3,
+      image: 'https://images3.penguinrandomhouse.com/cover/9781933771106',
+      name: 'History Revisited',
+      genre: 'Non-Fiction',
+      author: 'Emily Johnson',
+    },
+    {
+      id: 4,
+      image:
+        'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700317474i/202368563.jpg',
+      name: 'Culinary Delights',
+      genre: 'Cooking',
+      author: 'Michael Brown',
+    },
+  ];
 
-const App = () => {
-  return (
-    <div>
-      <h1>Digital Library</h1>
-      <div className="book-list">
-        {books.map((book, index) => (
-          <BookCard key={index} book={book} />
-        ))}
-      </div>
-    </div>
-  );
-};
+  return <>{/* write code to display all the books using */}</>;
+}
 
 export default App;
